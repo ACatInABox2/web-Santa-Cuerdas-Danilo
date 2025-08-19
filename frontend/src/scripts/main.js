77666c7af7d14b8ebefb79f40203bb2a36228521
ACatInABox2 <santiagouwu471@gmail.com>
2025-08-19 12:41:07 -0500

    estructura de archivos y arquitectura hecha, visual basica hecha, 3 secciones, pendiente a cambios.

---
+++ frontend/src/scripts/main.js
@@ -0,0 +1,18 @@
+//firma digital
+const firma_digital = {
+    nombre: "Santiago Torres",
+    email: "santiagomh912@gmail.com",
+    telefono: "+57 3216166273",
+    cargo: "Desarrollador Web"
+}
+console.log(firma_digital);
+// Cargar un componente en un div específico
+async function loadComponent(id, file) {
+  const res = await fetch(`components/${file}`);
+  document.getElementById(id).innerHTML = await res.text();
+}
+
+// Inyectar header y footer
+loadComponent("header", "header.html");
+loadComponent("footer", "footer.html");
+loadComponent("navbar", "navbar.html");
